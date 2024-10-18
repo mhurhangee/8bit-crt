ee6c3b1fa5fc9bab60326ec1735242d9b5ba9ad9
mhurhangee <m.hurhangee@me.com>
2024-10-18 11:16:57 +0100

    FIX: typo

--- components/CRTEffect.tsx
+++ components/CRTEffect.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState, useEffect, Suspense } from 'react'
+import React, { useState, useEffect } from 'react'
 import { usePathname } from 'next/navigation'
 import { EBButton } from "./EBButton"
 import {
